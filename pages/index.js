2b63413adaef83d5d80ddca761c5b4166e774e7b
Hilderal Térence <terencehilderal@gmail.com>
2021-08-30 21:30:58 +0200

    update getserversideprops

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ results }) {
 
 export async function getServerSideProps(context) {
 	const genre = context.query.genre;
-	console.log(requests[genre].url);
+
 	const request = await fetch(
 		`https://api.themoviedb.org/3${
 			requests[genre]?.url || requests.fetchTrending.url
